cd7dd1b62e8dd0818df63b6b1d3314760d6906ae
ncs-krishnavamshi <137599695+ncs-krishnavamshi@users.noreply.github.com>
2023-10-02 23:22:57 +0530

    Update copy2.sh

--- copy2.sh
+++ copy2.sh
@@ -4,29 +4,15 @@ sourceBucket="my-bucket1007"
 base_path="Software-Management/Installer"
 sourcePath="In-progress/6.4_Oct2023_R1"
 destinationPath="release"
-promoteChoice="$1"
-version="$2"
+version="$1"
 
-echo "promoteChoice: $promoteChoice"
-echo "version: $version"
+sourceFile="s3://${sourceBucket}/${base_path}/${sourcePath}/apache-maven-3.9.4-bin.tar.gz"
+destinationFile="s3://${sourceBucket}/${base_path}/${destinationPath}/${version}/apache-maven-3.9.4-bin.tar.gz"
 
-if [ "$promoteChoice" = "SNYPR" ]; then
-    sourceFile="s3://${sourceBucket}/${base_path}/${sourcePath}/${version}/apache-maven-3.9.4-bin.tar.gz"
-    destinationFile="s3://${sourceBucket}/${base_path}/${destinationPath}/${version}/"
-
-    aws s3 cp "$sourceFile" "$destinationFile" --recursive
-    exit_code=$?
-    if [ $exit_code -eq 0 ]; then
-        echo "Promoting $promoteChoice file of version $version"
-    else
-        echo "Failed to promote $promoteChoice file of version $version. Exit code: $exit_code"
-    fi
-elif [ "$promoteChoice" = "RIN" ]; then
-    # Uncomment and add the AWS S3 promotion logic for RIN files here
-    echo "Promoting $promoteChoice file of version $version"
-elif [ "$promoteChoice" = "RIN-Upgrade" ]; then
-    # Uncomment and add the AWS S3 promotion logic for RIN-Upgrade files here
-    echo "Promoting $promoteChoice file of version $version"
+aws s3 cp "$sourceFile" "$destinationFile"
+exit_code=$?
+if [ $exit_code -eq 0 ]; then
+    echo "Successfully copied file of version $version"
 else
-    echo "Invalid choice"
+    echo "Failed to copy file of version $version. Exit code: $exit_code"
 fi
