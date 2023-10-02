e445a29f001e9a4c9a064d34be93d3abf5bd9465
omeshvalyal <omeshvalyal@gmail.com>
2023-10-02 16:07:14 +0530

    Jenkins file updated

---
+++ copy2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Define your AWS credentials
+awsAccessKeyId="AKIA54ICCSKT2FLDLT56"
+awsSecretAccessKey="6naH/HRUHg4yVXSL4+CihgNcctcmgfD0inbGGJ3J"
+
+# Set AWS CLI environment variables
+export AWS_ACCESS_KEY_ID="$awsAccessKeyId"
+export AWS_SECRET_ACCESS_KEY="$awsSecretAccessKey"
+
+# Source and destination S3 bucket and paths
+sourceBucket="my-bucket1007"
+base_path="Software-Management/Installer/"
+sourcePath="In-progress/"
+destinationPath="release/"
+promoteChoice="$1"
+version="$2"
+echo "promoteChoice: $promoteChoice"
+echo "version: $version"
+if [ "$promoteChoice" = "SNYPR" ]; then
+    aws s3 cp "s3://${sourceBucket}/${base_path}/${sourcePath}/${version}/apache-maven-3.9.4-bin.tar.gz" "s3://${sourceBucket}/${base_path}/${destinationPath}/${version}/apache-maven-3.9.4-bin.tar.gz" --recursive
+    echo "Promoting $promoteChoice file of version $version"
+elif [ "$promoteChoice" = "RIN" ]; then
+    # aws s3 cp "s3://${sourceBucket}/${base_path}/${sourcePath}/${version}/apache-maven-3.9.4-bin.tar.gz" "s3://${sourceBucket}/${base_path}/${destinationPath}/${version}/apache-maven-3.9.4-bin.tar.gz" --recursive
+    echo "Promoting $promoteChoice file of version $version"
+elif [ "$promoteChoice" = "RIN-Upgrade" ]; then
+    # aws s3 cp "s3://${sourceBucket}/${base_path}/${sourcePath}/${version}/apache-maven-3.9.4-bin.tar.gz" "s3://${sourceBucket}/${base_path}/${destinationPath}/${version}/apache-maven-3.9.4-bin.tar.gz" --recursive
+    echo "Promoting $promoteChoice file of version $version"
+else
+    echo "Invalid choice"
+fi
